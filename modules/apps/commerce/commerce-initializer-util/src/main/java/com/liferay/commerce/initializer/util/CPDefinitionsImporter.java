380bac1311a379f107059ab8057f20d0b654be83
amosfong <amos.fong@liferay.com>
2022-03-14 11:05:32 -0700

    COMMERCE-8691 don't use sku as an externalReferenceCode for cpInstance cause it's not unique per company

--- modules/apps/commerce/commerce-initializer-util/src/main/java/com/liferay/commerce/initializer/util/CPDefinitionsImporter.java
+++ modules/apps/commerce/commerce-initializer-util/src/main/java/com/liferay/commerce/initializer/util/CPDefinitionsImporter.java
@@ -531,12 +531,6 @@ public class CPDefinitionsImporter {
 
 				cpInstance.setManufacturerPartNumber(manufacturerPartNumber);
 
-				String cpInstanceExternalReferenceCode =
-					_friendlyURLNormalizer.normalize(sku);
-
-				cpInstance.setExternalReferenceCode(
-					cpInstanceExternalReferenceCode);
-
 				_cpInstanceLocalService.updateCPInstance(cpInstance);
 
 				// Commerce warehouse items
@@ -877,8 +871,6 @@ public class CPDefinitionsImporter {
 		CPDefinition cpDefinition = _cpDefinitionLocalService.getCPDefinition(
 			cpDefinitionId);
 
-		String externalReferenceCode = _friendlyURLNormalizer.normalize(sku);
-
 		boolean overrideSubscriptionInfo = false;
 		boolean subscriptionEnabled = false;
 		int subscriptionLength = 0;
@@ -902,7 +894,7 @@ public class CPDefinitionsImporter {
 		}
 
 		CPInstance cpInstance = _cpInstanceLocalService.addCPInstance(
-			externalReferenceCode, cpDefinitionId, cpDefinition.getGroupId(),
+			StringPool.BLANK, cpDefinitionId, cpDefinition.getGroupId(),
 			sku, null, manufacturerPartNumber, true,
 			_cpDefinitionOptionRelLocalService.
 				getCPDefinitionOptionRelCPDefinitionOptionValueRelIds(

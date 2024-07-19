58aba4e1d294f384d724b9e6bf13de448585f4a9
amosfong <amos.fong@liferay.com>
2024-07-18 17:02:42 -0700

    LRSD-2768 fix logic

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/GenerateNewKey/pages/RequiredInformation.jsx
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/GenerateNewKey/pages/RequiredInformation.jsx
@@ -603,7 +603,7 @@ const RequiredInformation = ({
 								</div>
 							) : (
 								<div className="mx-6">
-									{!isComplimentaryKey || !isOemOrEnterprise && (
+									{!isComplimentaryKey && !isOemOrEnterprise && (
 										<div className="cp-input-generate-label">
 											<KeySelect
 												avaliableKeysMaximumCount={

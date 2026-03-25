30a5df88af76619658c6c6e28c8ec0d369300aa6
amosfong <amos.fong@liferay.com>
2026-03-25 12:41:49 -0700

    LRSD-8374 Copy main logic from talend

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-etc-spring-boot/src/main/java/com/liferay/customer/constants/ProductConstants.java
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-etc-spring-boot/src/main/java/com/liferay/customer/constants/ProductConstants.java
@@ -16,12 +16,78 @@ public class ProductConstants {
 	public static final String NAME_ADDITIONAL_STORAGE_100GB =
 		"Additional Storage (100GB)";
 
+	public static final String NAME_ANALYTICS_CLOUD_BASIC =
+		"Analytics Cloud Basic";
+
+	public static final String NAME_ANALYTICS_CLOUD_BUSINESS =
+		"Analytics Cloud Business";
+
+	public static final String NAME_ANALYTICS_CLOUD_ENTERPRISE =
+		"Analytics Cloud Enterprise";
+
+	public static final String NAME_BASIC_RESELLER_PARTNERSHIP =
+		"Basic Reseller Partnership";
+
+	public static final String NAME_DESIGNATED_CONTACT_ADD_ON =
+		"Designated Contact Add-on";
+
+	public static final String NAME_DISTRIBUTION_PARTNERSHIP =
+		"Distribution Partnership";
+
+	public static final String NAME_DXP_EWSA = "DXP Unlimited Enterprise-Wide";
+
+	public static final String NAME_DXP_FLEX = "DXP Flex";
+
+	public static final String NAME_DXP_OEM = "DXP OEM";
+
+	public static final String NAME_DXP_PRODUCTION = "DXP Production";
+
+	public static final String NAME_GLOBAL_24_7_SUPPORT = "Global 24/7 Support";
+
+	public static final String NAME_GOLD = "Gold Subscription";
+
+	public static final String NAME_LIFERAY_PAAS = "Liferay PaaS";
+
+	public static final String NAME_LIFERAY_PAAS_INSTANCE_PRODUCTION =
+		"Liferay PaaS Instance - Production";
+
+	public static final String NAME_LIFERAY_PAAS_SUBSCRIPTION_HA_PRODUCTION =
+		"Liferay PaaS Subscription HA Production";
+
+	public static final String NAME_LIFERAY_PAAS_SUBSCRIPTION_STD_PRODUCTION =
+		"Liferay PaaS Subscription Std Production";
+
+	public static final String NAME_LIFERAY_PLATFORM = "Liferay Platform";
+
+	public static final String NAME_LIFERAY_SAAS = "Liferay SaaS";
+
 	public static final String
 		NAME_LIFERAY_SAAS_100GB_EXTRA_STORAGE_DOCUMENT_LIBRARY =
 			"Liferay SaaS - 100GB Extra Storage (Document Library)";
 
 	public static final String NAME_LIFERAY_SAAS_BUSINESS_PLAN =
 		"Liferay SaaS - Business Plan";
+
+	public static final String NAME_LIFERAY_SAAS_CSP_CUSTOM_USER_TIER =
+		"Liferay SaaS - CSP - Custom User Tier";
+
+	public static final String NAME_LIFERAY_SAAS_CSP_UP_TO_1K_USERS =
+		"Liferay SaaS - CSP - Up to 1K Users";
+
+	public static final String NAME_LIFERAY_SAAS_CSP_UP_TO_5K_USERS =
+		"Liferay SaaS - CSP - Up to 5K Users";
+
+	public static final String NAME_LIFERAY_SAAS_CSP_UP_TO_10K_USERS =
+		"Liferay SaaS - CSP - Up to 10K Users";
+
+	public static final String NAME_LIFERAY_SAAS_CSP_UP_TO_20K_USERS =
+		"Liferay SaaS - CSP - Up to 20K Users";
+
+	public static final String NAME_LIFERAY_SAAS_CSP_UP_TO_100_USERS =
+		"Liferay SaaS - CSP - Up to 100 Users";
+
+	public static final String NAME_LIFERAY_SAAS_CSP_UP_TO_500_USERS =
+		"Liferay SaaS - CSP - Up to 500 Users";
 
 	public static final String NAME_LIFERAY_SAAS_CUSTOM_APVS =
 		"Liferay SaaS - Entitlements for Custom Quantity of APVs";
@@ -38,8 +104,86 @@ public class ProductConstants {
 	public static final String NAME_LIFERAY_SAAS_PRO_PLAN =
 		"Liferay SaaS - Pro Plan";
 
+	public static final String NAME_LIFERAY_SAAS_SUBSCRIPTION_ENGAGE_SITE =
+		"Liferay SaaS Subscription - Engage Site";
+
+	public static final String NAME_LIFERAY_SAAS_SUBSCRIPTION_SUPPORT_SITE =
+		"Liferay SaaS Subscription - Support Site";
+
+	public static final String NAME_LIFERAY_SAAS_SUBSCRIPTION_TRANSACT_SITE =
+		"Liferay SaaS Subscription - Transact Site";
+
+	public static final String NAME_LIMITED = "Limited Subscription";
+
+	public static final String NAME_MANAGED_SERVICES =
+		"Professional Services - Managed Services";
+
+	public static final String NAME_MANAGED_SERVICES_DXP =
+		"Managed Services for Liferay DXP - Self-Hosted";
+
+	public static final String NAME_MANAGED_SERVICES_LXC_SM =
+		"Managed Services for Liferay Experience Cloud - Self Managed";
+
+	public static final String NAME_PAAS_EXPERIENCE = "PaaS Experience";
+
+	public static final String NAME_PLATINUM = "Platinum Subscription";
+
+	public static final String NAME_PORTAL_EWSA = "Portal Enterprise";
+
+	public static final String NAME_PORTAL_OEM = "Portal OEM";
+
+	public static final String NAME_PORTAL_PRODUCTION = "Portal Production";
+
+	public static final String NAME_PREMIER_24_7_SUPPORT =
+		"Premier 24/7 Support";
+
+	public static final String NAME_PREMIUM = "Premium Subscription";
+
 	public static final String NAME_PRODUCTION_ENVIRONMENT =
 		"Production Environment";
+
+	public static final String NAME_RESELLER_PARTNERSHIP =
+		"Reseller Partnership";
+
+	public static final String NAME_SAAS_EXPERIENCE = "SaaS Experience";
+
+	public static final String NAME_SERVICE_PARTNERSHIP = "Service Partnership";
+
+	public static final String NAME_SERVICE_PARTNERSHIP_REFERRAL_PROGRAM =
+		"Service Partnership - Referral Program";
+
+	public static final String NAME_SILVER = "Silver Subscription";
+
+	public static final String NAME_SOLUTION_PARTNERSHIP_RESELLER_PROGRAM =
+		"Solution Partnership - Reseller Program";
+
+	public static final String NAME_STANDARD_8_5_SUPPORT =
+		"Standard 8/5 Support";
+
+	public static final String NAME_STRATEGIC_24_7_SUPPORT =
+		"Strategic 24/7 Support";
+
+	public static final String NAME_TRAINING_PARTNERSHIP =
+		"Training Partnership";
+
+	public static final String NAME_VALUE_ADDED_RESELLER_PARTNERSHIP =
+		"Value-Added Reseller Partnership";
+
+	public static final String[] NAMES_PARTNERSHIP = {
+		NAME_BASIC_RESELLER_PARTNERSHIP, NAME_DISTRIBUTION_PARTNERSHIP,
+		NAME_RESELLER_PARTNERSHIP, NAME_SERVICE_PARTNERSHIP,
+		NAME_SERVICE_PARTNERSHIP_REFERRAL_PROGRAM,
+		NAME_SOLUTION_PARTNERSHIP_RESELLER_PROGRAM, NAME_TRAINING_PARTNERSHIP,
+		NAME_VALUE_ADDED_RESELLER_PARTNERSHIP
+	};
+
+	public static final String[] NAMES_SUBSCRIPTION = {
+		NAME_GLOBAL_24_7_SUPPORT, NAME_GOLD, NAME_LIMITED, NAME_PLATINUM,
+		NAME_STANDARD_8_5_SUPPORT, NAME_STRATEGIC_24_7_SUPPORT,
+		NAME_PREMIER_24_7_SUPPORT, NAME_PREMIUM, NAME_SILVER
+	};
+
+	public static final String PURCHASE_STATUS_CANCELLED = "Cancelled";
 
 	public static final String[] SAAS_PLAN_SUBSCRIPTION_NAMES = {
 		NAME_LIFERAY_SAAS_BUSINESS_PLAN, NAME_LIFERAY_SAAS_ENTERPRISE_PLAN,

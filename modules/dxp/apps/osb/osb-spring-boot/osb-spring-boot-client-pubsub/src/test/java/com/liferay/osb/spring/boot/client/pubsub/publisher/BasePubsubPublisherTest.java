5b1d33da32fa97200c478b6217cf266d6bc540d0
Amos Fong <amos.fong@liferay.com>
2026-06-15 16:33:55 -0700

    LPD-89438 SF

--- modules/dxp/apps/osb/osb-spring-boot/osb-spring-boot-client-pubsub/src/test/java/com/liferay/osb/spring/boot/client/pubsub/publisher/BasePubsubPublisherTest.java
+++ modules/dxp/apps/osb/osb-spring-boot/osb-spring-boot-client-pubsub/src/test/java/com/liferay/osb/spring/boot/client/pubsub/publisher/BasePubsubPublisherTest.java
@@ -9,68 +9,51 @@ import com.liferay.osb.spring.boot.client.pubsub.Message;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * @author Kyle Bischof
  */
+@RunWith(MockitoJUnitRunner.class)
 public class BasePubsubPublisherTest {
 
 	@Test
 	public void testErrorHandling() throws Exception {
+		BasePubsubPublisher basePubsubPublisher = Mockito.mock(
+			BasePubsubPublisher.class, Mockito.CALLS_REAL_METHODS);
+
 		Exception exception = new Exception(
 			RandomStringUtils.randomAlphanumeric(16));
-
-		TestPubsubPublisher testPubsubPublisher = new TestPubsubPublisher(
-			exception);
-
 		Message message = new Message(
 			null, RandomStringUtils.randomAlphanumeric(10),
 			RandomStringUtils.randomAlphabetic(6));
 
-		testPubsubPublisher.publish(message);
+		Mockito.doNothing(
+		).when(
+			basePubsubPublisher
+		).handleError(
+			exception, message
+		);
 
-		Assert.assertSame(
-			exception, testPubsubPublisher.getHandledErrorException());
-		Assert.assertSame(
-			message, testPubsubPublisher.getHandledErrorMessage());
-	}
+		Mockito.doThrow(
+			exception
+		).when(
+			basePubsubPublisher
+		).doPublish(
+			message
+		);
 
-	private static class TestPubsubPublisher extends BasePubsubPublisher {
+		basePubsubPublisher.publish(message);
 
-		public TestPubsubPublisher(Exception exception) {
-			_exception = exception;
-		}
-
-		public Exception getHandledErrorException() {
-			return _handledErrorException;
-		}
-
-		public Message getHandledErrorMessage() {
-			return _handledErrorMessage;
-		}
-
-		@Override
-		protected void doPublish(Message message) throws Exception {
-			throw _exception;
-		}
-
-		@Override
-		protected String getProjectId() {
-			return "test-project";
-		}
-
-		@Override
-		protected void handleError(Message message, Exception exception) {
-			_handledErrorException = exception;
-			_handledErrorMessage = message;
-		}
-
-		private final Exception _exception;
-		private Exception _handledErrorException;
-		private Message _handledErrorMessage;
-
+		Mockito.verify(
+			basePubsubPublisher
+		).handleError(
+			exception, message
+		);
 	}
 
 }
